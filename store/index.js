25af6ae3042e56a97ca592a49f2bdae0e349853d
Hayao Sato <hayaos@jp.ibm.com>
2017-09-22 09:40:02 +0900

    Add store/index.js

--- store/index.js
+++ store/index.js
@@ -1,13 +1,32 @@
 import Vuex from 'vuex'; // eslint-disable-line import/no-extraneous-dependencies
 
 const store = () => new Vuex.Store({
-
   state: {
-    counter: 0,
+    authObject: {
+      username: '',
+      password: '',
+    },
+    isLogin: true,
+    workSpaceList: [],
+    currentWorkSpace: {},
   },
   mutations: {
-    increment(state) {
-      state.counter += 1; // eslint-disable-line no-param-reassign
+    setAuthObject(state, username, password) {
+      state.authObject.username = username;
+      state.authObject.password = password;
+      state.isLogin = true;
+    },
+    logout(state) {
+      state.authObject.username = '';
+      state.authObject.password = '';
+      state.isLogin = false;
+      state.workSpaceList = [];
+    },
+    setWorkSpaceList(state, workSpaceList) {
+      state.workSpaceList = workSpaceList;
+    },
+    setWorkSpace(state, workSpace) {
+      state.currentWorkSpace = workSpace;
     },
   },
 });
