285e94702e48fa9a8ca6c7a692a8f617c5adc92f
Hayao Sato <hayaos@jp.ibm.com>
2017-09-22 09:40:30 +0900

    Add header to app

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="appHeader">
+  <v-app id="inspire">
+    <v-toolbar fixed>
+      <v-toolbar-title>Watson Conversation Dialog Tester</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon v-if="$store.state.isLogin" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-items v-if="$store.state.isLogin" class="hidden-sm-and-down">
+        <v-select :items="$store.state.workSpaceList" v-model="selectedWorkSpace" label="Select" item-value="ddd">
+        </v-select>
+        <v-btn flat>Logout</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'appHeader',
+  data() {
+    return {
+      msg: 'Welcome to sample-app',
+      selectedWorkSpace: {},
+    };
+  },
+};
+</script>
+
+<style>
+</style>
