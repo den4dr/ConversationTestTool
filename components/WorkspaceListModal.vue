04614ea0fc372f4b51ea927c69cc9447d427349a
Hayao Sato <hayaos@jp.ibm.com>
2017-09-29 09:34:51 +0900

    Add modal

---
+++ components/WorkspaceListModal.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="workSpaceListModal">
+  <v-app>
+    <form>
+    </form>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'workSpaceListModal',
+  data() {
+    return {
+      selectedWorkSpace: {},
+    };
+  },
+  methods: {
+    submitWorkSpace(workSpace) {
+      this.$store.commit('setWorkSpaceList', response.workspaces);
+    }
+  },
+};
+</script>
