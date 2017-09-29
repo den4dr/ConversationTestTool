b9b56f5ba9da42d0393a7c06a63f474940ddac02
Hayao Sato <hayaos@jp.ibm.com>
2017-09-29 09:32:24 +0900

    Add vuex

--- store/index.js
+++ store/index.js
@@ -1,13 +1,46 @@
 import Vuex from 'vuex'; // eslint-disable-line import/no-extraneous-dependencies
 
 const store = () => new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
     authObject: {
       username: '',
       password: '',
+      area: '',
     },
     isLogin: true,
-    workSpaceList: [],
+    workSpaceList: [
+      {
+        name: 'Pizza app',
+        created: '2015-12-06T23:53:59.153Z',
+        language: 'en',
+        metadata: {},
+        updated: '2015-12-06T23:53:59.153Z',
+        description: 'Pizza app',
+        workspace_id: 'pizza_app-e0f3',
+        learning_opt_out: false,
+      },
+      {
+        name: 'Pizza app2',
+        created: '2015-12-06T23:53:59.153Z',
+        language: 'en',
+        metadata: {},
+        updated: '2015-12-06T23:53:59.153Z',
+        description: 'Pizza app2',
+        workspace_id: 'pizza_app-e0f6',
+        learning_opt_out: false,
+      },
+      {
+        name: 'Pizza app3',
+        created: '2015-12-06T23:53:59.153Z',
+        language: 'en',
+        metadata: {},
+        updated: '2015-12-06T23:53:59.153Z',
+        description: 'Pizza app4',
+        workspace_id: 'pizza_app-e0f9',
+        learning_opt_out: false,
+      },
+    ],
     currentWorkSpace: {},
   },
   mutations: {
